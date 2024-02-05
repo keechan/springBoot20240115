01f668816a06391cd77444ef3d1c4d3b24167aa5
keechan <keechan@live.co.kr>
2024-02-05 13:19:25 +0900

    20240205 두번째

---
+++ jungbo0129-1/src/main/java/com/rubypaper/Jungbo01291Application.java
@@ -0,0 +1,13 @@
+package com.rubypaper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jungbo01291Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jungbo01291Application.class, args);
+	}
+
+}
